d3c31ab07b754d562d8ec78e6f1592043883f267
LucasRazafinjatovo <96420763+LucasRazafinjatovo@users.noreply.github.com>
2024-02-03 14:01:32 +0300

    [Moteur] server

---
+++ m1p10mean-Lucas-Kanto/routes/routes.js
@@ -0,0 +1,6 @@
+var express = require('express');
+
+const router = express.Router();
+
+
+module.exports = router;
---
+++ m1p10mean-Lucas-Kanto/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', false);
+const routes = require('./routes/routes');
+const cors = require('cors');
+
+app.use(cors({
+  origin: "http://localhost:4200"
+}));
+
+const PORT = 3000;
+mongoose.connect("mongodb://localhost:27017/salon_beaute");
+
+
+const db = mongoose.connection;
+db.on('error', (error) => {
+    console.error('Error connecting to DB:', error);
+});
+
+db.once('open', () => {
+    console.log('-----Successfully connected to DB-------');
+    app.listen(PORT, () => {
+        console.log('Server started on port ${PORT}');
+    });
+});
+
+app.use(express.json());
+app.use(routes);
