27cdbc6441b787cd504c186dffe273411e5f2c83
LucasRazafinjatovo <96420763+LucasRazafinjatovo@users.noreply.github.com>
2024-02-05 15:03:14 +0300

    [Enlver id]

--- m1p10mean-Lucas-Kanto/src/service/ServiceModel.js
+++ m1p10mean-Lucas-Kanto/src/service/ServiceModel.js
@@ -1,10 +1,6 @@
 const mongoose = require('mongoose');
 
 const serviceSchema = new mongoose.Schema({
-  id: {
-    type: Number,
-    required: true,
-  },
   nom: {
     type: String,
     required: true,
