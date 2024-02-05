43c767b0401b3d1779387e4090683c551618720b
LucasRazafinjatovo <96420763+LucasRazafinjatovo@users.noreply.github.com>
2024-02-05 10:10:48 +0300

    [Service] image et data

--- m1p10mean-Lucas-Kanto/src/service/ServiceModel.js
+++ m1p10mean-Lucas-Kanto/src/service/ServiceModel.js
@@ -9,6 +9,10 @@ const serviceSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  image: {
+    type: String,
+    required: true,
+  },
   prix: {
     type: Number,
     required: true,
